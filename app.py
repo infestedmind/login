b0435b90c6b6c7093dc9d1f4a9b18d42467250e0
none <myinfestedmind@gmail.com>
2021-12-08 02:26:02 +0300

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import requests
+from flask import Flask, render_template, request, redirect
 import psycopg2
-from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+@app.route('/login/', methods=['GET'])
+def index():
+    return render_template('login.html')
 
 conn = psycopg2.connect(database='postgres', 
                         user='postgres', 
@@ -10,26 +15,40 @@ conn = psycopg2.connect(database='postgres',
                         port='5432')
 cursor = conn.cursor()
 
-@app.route('/login/', methods=['GET'])
-def index():
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            try:
+                cursor.execute("SELECT * FROM service.users WHERE login=%s AND pass=%s",
+                               (str(username), str(password)))
+                records = list(cursor.fetchall())
+                return render_template('account.html', full_name=records[0][1])
+            except:
+                return "Введите корректные данные"
+
+        elif request.form.get("registration"):
+            return redirect("/registration/")
     return render_template('login.html')
 
-@app.route('/login/', methods=['POST'])
-def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    cursor.execute("SELECT * FROM service.users WHERE login=%s AND pass=%s", 
-                    (str(username), str(password)))
-    records = list(cursor.fetchall())
-    match (username, password):
-        case ('', ''):
-            return "ВВЕДИТЕ ДАННЫЕ" 
-        case (username, ''):
-            return "ВВЕДИТЕ ПАРОЛЬ"
-        case ('', password):
-            return "ВВЕДИте ЛОГИН"
-        case (username, password):
-            if not records:
-                return ('Неверное имя пользователя или пароль')
-            else:
-                return render_template('account.html', full_name=records[0][1])
+
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        if not name.replace(' ', '').isalpha():
+            return 'Нельзя использовать цифры в имени'
+        login = request.form.get('login')
+        password = request.form.get('password')
+        cursor.execute("SELECT * FROM service.users WHERE login=%s",
+        (str(login),))
+        if not cursor.fetchone():
+            cursor.execute('INSERT INTO service.users (fullname, login, pass) VALUES (%s, %s, %s);',
+            (str(name), str(login), str(password)))
+            conn.commit()
+        else:
+            return "Вы уже зарегистрированны"
+        return redirect('/login/')
+    return render_template('registration.html')
